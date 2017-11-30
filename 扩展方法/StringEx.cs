083b6bf5a25f66453a6a145fefd738a656f61bda
海东 <sugartomato@msn.com>
2017-11-30 08:21:02 +0800

    添加String类型扩展

---
+++ 扩展方法/StringEx.cs
@@ -0,0 +1,22 @@
+﻿namespace 扩展方法
+{
+    using System;
+
+    /// <summary>
+    /// String扩展方法
+    /// </summary>
+    public static class StringEx
+    {
+
+        /// <summary>
+        /// 验证字符串是否为手机号
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static Boolean IsCellPhone(this String str)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(str, "^[1]{1}[0-9]{2}[0-9]{8}$");
+        }
+
+    }
+}
---
+++ 扩展方法Tests/StringExTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 扩展方法;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 扩展方法.Tests
+{
+    [TestClass()]
+    public class StringExTests
+    {
+        [TestMethod()]
+        public void IsCellPhoneTest()
+        {
+            Console.WriteLine("13111211355".IsCellPhone());
+            Console.WriteLine("123".IsCellPhone());
+            Console.WriteLine("111111111111".IsCellPhone());
+        }
+    }
+}
